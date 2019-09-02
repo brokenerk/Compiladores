096ce456e5a937a35c0af09571678bf8e8c71eda
abiisnn <abigail.nic.say@hotmail.com>
2019-09-02 08:34:11 -0500

    ok

---
+++ App/static/js/show.js
@@ -0,0 +1,14 @@
+// To show forms based in the operation
+  var cont = 0;
+  var y;
+  function show(x) {
+    cont++;
+    if(cont == 1) { 
+      var z = document.getElementById("Add"); 
+      y = z;
+    }
+    y.style.display = "none";
+    if(x.style.display == "none") { x.style.display = "block"; } 
+    else { x.style.display = "block"; }
+    y = x;
+  }
