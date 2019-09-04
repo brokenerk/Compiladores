b4f230c96390e1e8c9a18e0ee501ef4bdb4ce03d
abiisnn <abigail.nic.say@hotmail.com>
2019-09-04 12:37:23 -0500

    Ok

--- App/app.py
+++ App/app.py
@@ -12,6 +12,12 @@ import forms
 
 app = Flask(__name__)
 
+# Manage every AFN that has been created
+afnAvailable = {}
+alphabet = {}
+idAfnAvailable = {}
+cont = 0
+
 @app.route("/")
 def index():
 	return render_template('home.html')
@@ -22,17 +28,62 @@ def aboutUs():
 
 @app.route("/convert", methods = ['GET', 'POST'])
 def convert():
-	add_form = forms.AddForm(request.form)
-	global afnAvailable
-	if request.method == 'POST' and add_form.validate():
-		print("Yes ")
-		print(add_form.char.data)
-		c = add_form.char.data
-		afn = AFN.createBasic(c)
-		afn.display()
+	addForm = forms.AddForm(request.form)
+	afn1 = request.form.get('afn1')
+	afn2 = request.form.get('afn2')
+	opt = request.form.get('option')
+	print("-------------------------------------------------- OPTION:", opt)
 
-	
-	return render_template('convert.html', form = add_form)
+	if request.method == 'POST' and opt == "0":
+			c = addForm.char.data
+			if c not in alphabet:
+				afn = AFN.createBasic(c)
+				afnAvailable[afn.getId()] = afn
+				alphabet[c] = afn.getId()
+				idAfnAvailable[afn.getId()] = afn.getId()
+				print("SIZE:", len(afnAvailable))
+				
+	elif request.method == 'POST' and opt == "1":
+		if len(afn1) > 0 and len(afn2) > 0:
+			newAfn = afnAvailable[int(afn1)].join(afnAvailable[int(afn2)])
+			afnAvailable[newAfn.getId()] = newAfn
+			idAfnAvailable[newAfn.getId()] = newAfn.getId()
+			print("SIZE:", len(afnAvailable))
+
+	elif request.method == 'POST' and opt == "2":
+		if len(afn1) > 0 and len(afn2) > 0:
+			newAfn = afnAvailable[int(afn1)].concat(afnAvailable[int(afn2)])
+			afnAvailable[newAfn.getId()] = newAfn
+			idAfnAvailable[newAfn.getId()] = newAfn.getId()
+			print("SIZE:", len(afnAvailable))
+
+	elif request.method == 'POST' and opt == "3":
+		if len(afn1) > 0:
+			newAfn = afnAvailable[int(afn1)].positiveClosure()
+			afnAvailable[newAfn.getId()] = newAfn
+			idAfnAvailable[newAfn.getId()] = newAfn.getId()
+			print("SIZE:", len(afnAvailable))
+
+	elif request.method == 'POST' and opt == "4":
+		if len(afn1) > 0:
+			newAfn = afnAvailable[int(afn1)].kleeneClosure()
+			afnAvailable[newAfn.getId()] = newAfn
+			idAfnAvailable[newAfn.getId()] = newAfn.getId()
+			print("SIZE:", len(afnAvailable))
+
+	elif request.method == 'POST' and opt == "5":
+		if len(afn1) > 0:
+			newAfn = afnAvailable[int(afn1)].epsilonClosure()
+			afnAvailable[newAfn.getId()] = newAfn
+			idAfnAvailable[newAfn.getId()] = newAfn.getId()
+			print("SIZE:", len(afnAvailable))
+
+	elif request.method == 'POST' and opt == "6":
+		if len(afn1) > 0:
+		    print("Other option")
+
+	return render_template('convert.html', add=addForm, idAfn=idAfnAvailable)
+
 
 if __name__ == '__main__':
 	app.run(debug = True, port = 5000)
--- App/forms.py
+++ App/forms.py
@@ -1,13 +1,11 @@
 from wtforms import Form 
-from wtforms import StringField, TextField
+from wtforms import StringField, TextField, SelectField
 from wtforms.fields.html5 import EmailField
 from wtforms import validators
 
 class AddForm(Form):
-	char = StringField('Name ',
-		[ 
-			validators.length(min=1, max=5, message='No valid!'),
-			validators.Required(message="We need it.")
+	char = StringField('Name ', 
+		[
+			validators.length(min=1, max=1)
 		]
 		)
-	
