1f6974d21f7ff757bf1e9b5b01bdaa715cface4e
abiisnn <abigail.nic.say@hotmail.com>
2019-09-08 22:52:27 -0500

    Almost

--- App/static/js/show.js
+++ App/static/js/show.js
@@ -2,13 +2,13 @@
   var cont = 0;
   var y;
   function show(x) {
-    cont++;
-    if(cont == 1) { 
-      var z = document.getElementById("Add"); 
-      y = z;
-    }
-    y.style.display = "none";
-    if(x.style.display == "none") { x.style.display = "block"; } 
-    else { x.style.display = "block"; }
-    y = x;
+    x.style.display = "block";
+    // cont++;
+    // if(cont == 1) { 
+    //   var z = document.getElementById("Add"); 
+    //   y = z;
+    // }
+    // y.style.display = "none";
+    // if(x.style.display == "none") { x.style.display = "block"; } 
+    // else { x.style.display = "block"; }
   }
