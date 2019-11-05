2b00dfe31bcb63d1fd9cc92f9e6ac19aa37d9e1e
RandyViG <rvillanuevag1603@alumno.ipn.mx>
2019-11-04 22:38:28 -0600

    Calculator ready 'stage 2'

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,13 +1,11 @@
-# -----------------------------------------------------------------------------
-# Run: sudo python3 calc.py
-# A simple calculator with variables.
-# -----------------------------------------------------------------------------
+#!Python3
 
 tokens = (
-        'NAME','NUMBER',
-        'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-        'LPAREN','RPAREN',
-        )
+    'NAME','INTEGER','FLOAT',
+    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
+    'LPAREN','RPAREN',
+    )
+
 # Tokens
 
 t_PLUS    = r'\+'
@@ -19,9 +17,14 @@ t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
 t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
-def t_NUMBER(t):
-    r'\d+|(\d+\.\d+)'
+def t_FLOAT(t):
+    r'\d+\.\d+'
     t.value = float(t.value)
+    return t
+
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)
     return t
 
 # Ignored characters
@@ -41,12 +44,13 @@ lex.lex()
 
 # Precedence rules for the arithmetic operators
 precedence = (
-            ('left','PLUS','MINUS'),
-            ('left','TIMES','DIVIDE'),
-            ('right','UMINUS'),
-            )
+    ('left','PLUS','MINUS'),
+    ('left','TIMES','DIVIDE'),
+    ('right','UMINUS'),
+    )
 
 # dictionary of names (for storing variables)
+names = { }
 
 def p_statement_assign(p):
     'statement : NAME EQUALS expression'
@@ -74,8 +78,12 @@ def p_expression_group(p):
     'expression : LPAREN expression RPAREN'
     p[0] = p[2]
 
-def p_expression_number(p):
-    'expression : NUMBER'
+def p_expression_float(p):
+    'expression : FLOAT'
+    p[0] = p[1]
+
+def p_expression_integer(p):
+    'expression : INTEGER'
     p[0] = p[1]
 
 def p_expression_name(p):
@@ -94,7 +102,7 @@ yacc.yacc()
 
 while True:
     try:
-        s = input('calc > ')   # use input() on Python 3
+        s = input('calc > ')
     except EOFError:
         break
     yacc.parse(s)
